17b225ff8a384e674e33fea021d4a35dc2225c60
muklisaprilian <muklisapriliansyah@gmail.com>
2024-11-24 21:20:42 +0700

    edit

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 
 module.exports = {
     APP_PORT: process.env.APP_PORT ?? 3000,
-    DB_HOST: process.env.DB_HOST ?? "sql.freedb.tech",
-    DB_NAME: process.env.DB_NAME ?? "freedb_dbppob",
-    DB_USER: process.env.DB_USER ?? "freedb_rootppob",
+    DB_HOST: process.env.DB_HOST ?? "localhost",
+    DB_NAME: process.env.DB_NAME ?? "db_ppob",
+    DB_USER: process.env.DB_USER ?? "root",
     DIALECT: process.env.DB_DIALECT ?? "mysql",
-    DB_PASSWORD: process.env.DB_PASSWORD ?? "rCF4bveDnF!WW4w",
+    DB_PASSWORD: process.env.DB_PASSWORD ?? "P@55w0rd",
     //
-    host : "sql.freedb.tech",
+    host : "localhost",
     port : 3306,
-    user : "freedb_rootppob",
-    password: "rCF4bveDnF!WW4w",
-    database: "freedb_dbppob", 
+    user : "root",
+    password: "P@55w0rd",
+    database: "db_ppob", 
     dialect: "mysql",
     key: "P$55w0rd$123",
 }
